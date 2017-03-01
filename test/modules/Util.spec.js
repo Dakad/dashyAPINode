0cd4f0ac886af5fc2333c46424cc39ee03bdb32b
David <david@Sebastiens-MacBook-Pro.local>
2017-03-01 15:39:58 +0100

    ESLint error on .test

--- test/modules/Util.spec.js
+++ test/modules/Util.spec.js
@@ -18,9 +18,22 @@ const Util = require('../../src/modules/Util');
 
 describe('Module : Util', () => {
   describe('isEmptyOrNull', () => {
-    it('should return true', () => {
-      const isEmpty = Util.isEmptyOrNull();
-      expect(isEmpty).to.be.true;
+    it('should return true', (done) => {
+      expect(Util.isEmptyOrNull()).to.be.true;
+      expect(Util.isEmptyOrNull(undefined)).to.be.true;
+      expect(Util.isEmptyOrNull(null)).to.be.true;
+      expect(Util.isEmptyOrNull({})).to.be.true;
+      expect(Util.isEmptyOrNull([])).to.be.true;
+
+      done();
+    });
+
+    it('should return false', () => {
+      expect(Util.isEmptyOrNull({ a: 1 })).to.be.false;
+      expect(Util.isEmptyOrNull([1])).to.be.false;
+      expect(Util.isEmptyOrNull({ a: 1, b: 2 })).to.be.false;
+      expect(Util.isEmptyOrNull([1, 2, 3])).to.be.false;
+      
     });
   });
 });

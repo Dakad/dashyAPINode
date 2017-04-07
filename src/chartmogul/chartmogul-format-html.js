ff00010a6530b93f9f7bc4803866c30863e02c86
A. David <david.adegnon@vinci.be>
2017-04-08 00:07:31 +0200

    Quick fix

--- src/chartmogul/chartmogul-format-html.js
+++ src/chartmogul/chartmogul-format-html.js
@@ -34,8 +34,7 @@ const Util = require('../components/util');
 class CharMogulHTMLFormatter {
 
   /**
-   * @static
-   * @private
+   * @private @static
    * Generate <img> with the corresponding country.
    *
    * @param {string} isoCountry The country in ISO-3166 format.
@@ -45,13 +44,12 @@ class CharMogulHTMLFormatter {
    * @memberOf CharMogulHTMLFormatter
    */
   static generateFlagImg(isoCountry) {
-    const srcLink = ':' + Config.api.port + '/assets/img/flags';
+    const srcLink = Config.api.host + '/assets/img/flags';
     return `
-        <img src="${srcLink}/${isoCountry.toLowerCase()}.png" 
+        <img src="${srcLink}/${isoCountry}.png" 
              alt="${Countries[isoCountry]}" 
-             style="width:100;height:100;"
-        >
-    `.trim();
+             style="width:50;height:50;"
+        >`.trim();
   }
 
   /**
@@ -75,14 +73,16 @@ class CharMogulHTMLFormatter {
     const isISO3166 = (when) => where.length === 2;
 
     if (when instanceof Date) {
-      when = toLocaleString.call(when, 'fr-BE');
+      when = toLocaleString.call(when, 'fr-BE')
+              .replace(' GMT+0200 (CEST)', '');
     }
     return `<h1><strong>${who}</strong></h1>
               <h2>
-                When ? : <strong><ins> ${when}</ins></strong>
+                When ? : <b><u>${when}</u></b>
                 </h2>
+              ${!isISO3166(where) ? 'Where ?'
+              : CharMogulHTMLFormatter.generateFlagImg(where) }
             <h2> 
-              ${!isISO3166(where) ? generateFlagImg(where) : 'Where ?'}
               <strong>
                 <ins>${isISO3166(where) ? Countries[where] : where}</ins>
               </strong>
@@ -90,12 +90,27 @@ class CharMogulHTMLFormatter {
               ${ (mrr) ?
         `<hr>
         <h3>
-                  MRR : <strong>
-                          <ins>${Util.toMoneyFormat(mrr / 100)}</ins>
-                        </strong>
-                  </h3>`
+        MRR : <strong>
+                <ins>${Util.toMoneyFormat(mrr / 100)}</ins>
+              </strong>
+        </h3>`
         : ''}
             `.replace(/[\r\n]/g, '');
+
+            /*<h1  style="margin: 5px 10px 10px;"><b><u>My website name</u></b></h1>
+
+<div>
+  <img src="https://avatars0.githubusercontent.com/u/5960567?v=3&s=60g" alt="Flag of " 
+        style="float:left;width: 130px;height: 110px;margin: 0 10px;"
+  />
+  <h2><img src="https://cdn2.iconfinder.com/data/icons/snipicons/500/time-32.png" alt="DateTime" style="float:left;width: 25px;height: 25px;margin: 0 5px;"/><b>Fri Apr 07 2017 15:00:00</b></h2>
+<br>
+<h2><img src="https://cdn0.iconfinder.com/data/icons/shift-free/32/Currency_Euro-32.png" alt="MRR" style="float:left;width: 25px;height: 25px;margin: 0 5px;"/><b>2199,65</b></h2>
+<br>
+<h2><img src="https://cdn0.iconfinder.com/data/icons/gcons-2/21/world7-48.png" alt="Where" style="float:left;width: 25px;height: 25px;margin: 0 5px;"/><b>Country</b></h2>
+
+</div>*/
+
   }
 
 }

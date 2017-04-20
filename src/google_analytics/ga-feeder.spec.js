ac3d192140a2026b7d94cae249e506432853af99
David Stagiaire <david.adegnon@apptweak.com>
2017-04-20 13:09:22 +0200

    New branch for the Google Analytics part

---
+++ src/google_analytics/ga-feeder.spec.js
@@ -0,0 +1,42 @@
+/**
+ * Unit Test  for the GoogleAnalyticsFeed.
+ */
+
+
+// -------------------------------------------------------------------
+// Dependencies
+
+// Packages
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+// const httpMocks = require('node-mocks-http');
+// const sinon = require('sinon');
+// const request = require('superagent');
+// const mockRequest = require('superagent-mock');
+
+// Built-in
+
+// Mine
+// const Config = require('../../config/test');
+// const Util = require('../components/util');
+// const GAFeed = require('./ga-feed');
+// const mockReqConf = require('./ga-superagent-mock-config');
+
+
+// -------------------------------------------------------------------
+// Properties
+// let feed = new GAFeed();
+chai.use(chaiAsPromised);
+// const {expect} = chai;
+
+// let spyFeedReqChartMogul;
+// let superagentMock = mockRequest(request, mockReqConf,
+//   ({method, url}) => console.log('superagentMock call', method, url));
+
+// -------------------------------------------------------------------
+// Test Units
+
+
+describe('ChartMogul : Feeder', () => {
+  beforeEach(() => { });
+});

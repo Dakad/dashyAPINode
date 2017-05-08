6631e8571423bc8061d2a020315eb0f6ed2a4671
David Stagiaire <david.adegnon@apptweak.com>
2017-05-08 18:10:48 +0200

    Correct JSDoc for components/util

--- src/components/util.js
+++ src/components/util.js
@@ -1,8 +1,6 @@
 /**
  * @overview  The Util Conponent.
  * @module components/util
- * @requires config
- * @requires util
  */
 
 
@@ -30,7 +28,8 @@ module.exports = class Util {
    * Check if the given Object is undefined, null or empty.
    *
    * @param {Object} obj - The object to check.
-   * @return {Boolean}
+   * @return {Boolean} **true** if obj is defined and contains something
+   *  otherwise **false**
    */
   static isEmptyOrNull(obj) {
     return !obj || Object.keys(obj).length === 0;
@@ -38,9 +37,9 @@ module.exports = class Util {
 
 
   /**
-   * Convert a date into ISO 8601 date format but only in format YYYY-MM-DD.
+   * Convert a date into ISO 8601 format keep only YYYY-MM-DD part.
    *
-   * @param {Date|string} date - The normal date,
+   * @param {Date|string|number} [date=new Date()] - The normal date or in ms.
    * @return {String} The formatted string corresponding to the date or if not
    *    provided use the current date.
    */
@@ -57,15 +56,14 @@ module.exports = class Util {
 
 
   /**
-   * Convert the num into a EUR currency format.
+   * Convert a number into a EUR currency format.
    *
-   * @static
-   * @param {number} num - The Number to format.
-   * @param {character} delimiter - The thousand delimiter
-   * @param {character} separator - The Unit separator
+   * @param {number} [num=0] - The Number to format.
+   * @param {char} [delimiter='<SPACE>'] - The thousand delimiter
+   * @param {char} [separator='.'] - The Unit separator
    * @return {string} a formatted string corresponding to the currency.
    */
-  static toMoneyFormat(num, delimiter = ' ', separator = '.') {
+  static toMoneyFormat(num=0, delimiter = ' ', separator = '.') {
     // return Number(num.toFixed(2)).toLocaleString('en-GB', {
     //   style: 'currency',
     //   currency: 'EUR',
@@ -76,14 +74,15 @@ module.exports = class Util {
   }
 
   /**
+   * Generate a hash code for a given object. Would return the same integer
+   * for the same object.
+   * If the given src object is **null**, will return 0;
+   *
    * @param {string|Array|Object} str - The src to hash
-   * @return {number} Hascode
+   * @return {number} Hashcode value for this object
    */
   static hashCode(str) {
-    if (!str ||
-      (Array.isArray(str) && str.length === 0) ||
-      Object.keys(str).length === 0
-    ) {
+    if (!str || Util.isEmptyOrNull(src)) {
       return 0;
     }
 
@@ -97,36 +96,11 @@ module.exports = class Util {
       .reduce((hash, c) => ((hash << 5) - hash) + c.charCodeAt(0), 0);
   };
 
-
   /**
-   * @param {string} str - The src to hash
-   * @return {string} hex color
-   */
-  static hashColor(str) {
-    const hex = (Util.hashCode(str) & 0x00FFFFFF)
-      .toString(16)
-      .toUpperCase();
-    return '#' + '00000'.substring(0, 6 - hex.length) + hex;
-  }
-
-  //
-  //  * Convert an array into an object.
-  //  *
-  //  * @param {Array} keys - The keys for the generated object.
-  //  * @param {Array} arr - The array source.
-  //  *
-  //  * @return  {Object} An array mapped by his keys or passed in args.
-  //  */
-  // // static convertArrayToObject(keys = [], arr = []) {
-  //   return Object.keys(arr).reduce((pms, p, i) => arr[p], {});
-  //   // const param = arr[p];
-  // };
-
-
-  /**
+   * Get the full country for a given ISO code.
+   * Will return **null** if the code is invalid.
    *
-   * @static
-   * @param {any} iso - The ISO3166-1-Alpha-2 code of the country.
+   * @param {string} iso - The ISO3166-1-Alpha-2 code of the country.
    * @return {Object} The country corresponding to the ISO.
    */
   static getCountryFromISOCode(iso) {
@@ -138,64 +112,15 @@ module.exports = class Util {
   }
 
   /**
-   * Check if the params, if defined, are valid.
-   * @param {Object} params - The params sent in the req.
-   * Also accept the req.query.
-   *
-   * @return {Object} Contains a boolean for inValid and a string errMsg.
-   */
-  /*
-    static checkParams(params) {
-      const validParams = Config.api.validParams;
-      const check = {
-        isValid: true,
-        errMsg: null,
-      };
-
-      if (!Util.isEmptyOrNull(params)) {
-        // First , check if that param exists
-        Object.keys(params)
-          .filter((param) => Object.keys(validParams).indexOf(param) !== -1)
-          .some((param) => { // Then, check if valid
-            let paramValue = params[param];
-            switch (param) {
-              default: // This params'value is enumerated;
-                const validParamValues = validParams[param];
-              check.isValid = (validParamValues.indexOf(paramValue) !== -1);
-              break;
-              case 'for': // param for must be a Number
-                  if (typeof params.for === 'string') {
-                    params.for = Number.parseInt(params.for, 10);
-                  }
-                check.isValid = (params.for > 0 && params.for <= 12);
-                break;
-            }
-
-            if (!check.isValid) {
-              let msg = Config.api.msg.err.check[param];
-              if (!msg) {
-                msg = Config.api.msg.err.check._def;
-                msg = util_.format(msg, param, paramValue);
-              }
-              check.errMsg = msg;
-            }
-
-            return !check.isValid; // Stop  the loop if some param is not valid.
-          });
-      }
-
-      return check;
-    }
-  */
-
-
-  /**
    * Retrieve all necesary dates.
    *
-   * @private
-   * @static
    * @param {string} [type] - Only the kind of date
-   * @return {Object} dates - All dates
+   * @return {Object} dates - All dates.
+   * @return {Object} dates.firstInPastMonth - The 1st day in previous month.
+   * @return {Object} dates.dateInPastMonth - The same date in previous month.
+   * @return {Object} dates.endInPastMonth - The last day in previous month.
+   * @return {Object} dates.firstInMonth - The 1st day in this month.
+   * @return {Object} dates.today - The today date.
    */
   static getAllDates(type) {
     // The first day in the prev. month at 00:00:00:00
@@ -238,18 +163,18 @@ module.exports = class Util {
 
 
   /**
-   * Convert sec to time format MMSS.
+   * Convert seconds to time format HHMMSS.
+   * If the parameter is not given, will use Date.now().
+   * If the parameter is not valid, will use 0.
    *
-   * @static
-   *
-   * @param {null|number|string} [sec] - The number of seconds
+   * @param {number|string} [sec=Date.now()] - The number of seconds
    *  if null, use the current time,
    * @return {Object} hhmmss - Contains the duration in time,
    * @return {Object} hhmmss.time - The time of duration
    * @return {Object} hhmmss.h - The number of hours.
    * @return {Object} hhmmss.m - The number of minutes.
    * @return {Object} hhmmss.s - The number of seconds.
-   * @return {Object} hhmmss.format - The formatted time '%$M%m %$S%s'
+   * @return {Object} hhmmss.format - The formatted time '%$HH%h %$MM%m %$SS%s'
    */
   static toHHMMSS(sec) {
     let duration = Date.now();
@@ -279,5 +204,81 @@ module.exports = class Util {
       format,
     };
   }
+
+
+  // /**
+  //  * @param {string} str - The src to hash
+  //  * @return {string} hex color
+  //  */
+  // static hashColor(str) {
+  //   const hex = (Util.hashCode(str) & 0x00FFFFFF)
+  //     .toString(16)
+  //     .toUpperCase();
+  //   return '#' + '00000'.substring(0, 6 - hex.length) + hex;
+  // }
+
+  //
+  //  * Convert an array into an object.
+  //  *
+  //  * @param {Array} keys - The keys for the generated object.
+  //  * @param {Array} arr - The array source.
+  //  *
+  //  * @return  {Object} An array mapped by his keys or passed in args.
+  //  */
+  // // static convertArrayToObject(keys = [], arr = []) {
+  //   return Object.keys(arr).reduce((pms, p, i) => arr[p], {});
+  //   // const param = arr[p];
+  // };
+
+
+  // /**
+  //  * Check if the params, if defined, are valid.
+  //  * @param {Object} params - The params sent in the req.
+  //  * Also accept the req.query.
+  //  *
+  //  * @return {Object} Contains a boolean for inValid and a string errMsg.
+  //  */
+  //   static checkParams(params) {
+  //     const validParams = Config.api.validParams;
+  //     const check = {
+  //       isValid: true,
+  //       errMsg: null,
+  //     };
+
+  //     if (!Util.isEmptyOrNull(params)) {
+  //       // First , check if that param exists
+  //       Object.keys(params)
+  //         .filter((param) => Object.keys(validParams).indexOf(param) !== -1)
+  //         .some((param) => { // Then, check if valid
+  //           let paramValue = params[param];
+  //           switch (param) {
+  //             default: // This params'value is enumerated;
+  //               const validParamValues = validParams[param];
+  //             check.isValid = (validParamValues.indexOf(paramValue) !== -1);
+  //             break;
+  //             case 'for': // param for must be a Number
+  //                 if (typeof params.for === 'string') {
+  //                   params.for = Number.parseInt(params.for, 10);
+  //                 }
+  //               check.isValid = (params.for > 0 && params.for <= 12);
+  //               break;
+  //           }
+
+  //           if (!check.isValid) {
+  //             let msg = Config.api.msg.err.check[param];
+  //             if (!msg) {
+  //               msg = Config.api.msg.err.check._def;
+  //               msg = util_.format(msg, param, paramValue);
+  //             }
+  //             check.errMsg = msg;
+  //           }
+
+  //           return !check.isValid; // Stop  the loop if some param is not valid.
+  //         });
+  //     }
+
+  //     return check;
+  //   }
+
 
 };

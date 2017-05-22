6b1951bd724eec5dca027f4a84a601769d0fa21e
A. David <david.adegnon@vinci.be>
2017-05-22 14:05:44 +0000

    Add file for Docker image

--- scripts/install-node-npm.sh
+++ scripts/install-node-npm.sh
@@ -1,9 +1,9 @@
 echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
 . ~/.bashrc
 mkdir ~/local
-mkdir ~/node-v7.8
-cd ~/node-v7.8
-curl http://nodejs.org/dist/v7.8.0/node-v7.8.0.tar.gz | tar xz --strip-components=1
+mkdir ~/node-v7.10
+cd ~/node-v7.10
+curl http://nodejs.org/dist/v7.10.0/node-v7.10.0.tar.gz | tar xz --strip-components=1
 ./configure --prefix=~/local
 make install
-curl http://npmjs.org/install.sh | sh
+bash ./npm_install.sh | sh
